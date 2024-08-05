77e6ac4fa33457509446c88a51f9c6f4e7d7df2a
Kamil Mrowka <kamil@chicos.life>
2024-08-05 09:49:21 +0200

    Add knex.js

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+
+const host = process.env.MYSQL_HOST;
+const user = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWD;
+const db = process.env.MYSQL_DATABASE;
+
+export default {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: host,
+      port: 3306,
+      user: user,
+      password: password,
+      database: db,
+    },
+  },
+};
